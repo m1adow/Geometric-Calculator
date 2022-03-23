7ca007aa00f3623f1d909e995a05bdb9da8464a4
m1adow <Vell0505189970sl@gmail.com>
2022-03-23 20:22:47 +0200

    Added more trapezoids classes

---
+++ Geometric Calculator/Models/Quadrangles/Trapezoids/RightTrapezoid.cs
@@ -0,0 +1,9 @@
+﻿namespace Geometric_Calculator.Models.Quadrangles.Trapezoids;
+
+public class RightTrapezoid : Trapezoid
+{
+    public RightTrapezoid(double smallWarp, double bigWarp, double firstSide, double secondSide, double thirdAngle, double fourthAngle, double firstDiagonal, double secondDiagonal, double firstHeight, double secondHeight, double thirdHeight, double fourthHeight) : base(smallWarp, bigWarp, firstSide, secondSide, 90, 90, thirdAngle, fourthAngle, firstDiagonal, secondDiagonal, firstHeight, secondHeight, thirdHeight, fourthHeight)
+    {
+
+    }
+}
---
+++ Geometric Calculator/Models/Quadrangles/Trapezoids/TrueTrapezoid.cs
@@ -0,0 +1,9 @@
+﻿namespace Geometric_Calculator.Models.Quadrangles.Trapezoids;
+
+public class TrueTrapezoid : Trapezoid
+{
+    public TrueTrapezoid(double smallWarp, double bigWarp, double side, double firstAngle, double secondAngle, double diagonal, double firstHeight, double secondHeight, double thirdHeight, double fourthHeight) : base(smallWarp, bigWarp, side, side, firstAngle, firstAngle, secondAngle, secondAngle, diagonal, diagonal, firstHeight, secondHeight, thirdHeight, fourthHeight)
+    {
+
+    }
+}
