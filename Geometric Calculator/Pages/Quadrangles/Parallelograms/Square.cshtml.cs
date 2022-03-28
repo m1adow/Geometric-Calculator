0a4ed6286e1b7770562b443bd8b4fd466e6540a1
m1adow <Vell0505189970sl@gmail.com>
2022-03-28 23:26:55 +0300

    Added square handler

--- Geometric Calculator/Pages/Quadrangles/Parallelograms/Square.cshtml.cs
+++ Geometric Calculator/Pages/Quadrangles/Parallelograms/Square.cshtml.cs
@@ -1,3 +1,4 @@
+using Geometric_Calculator.Models;
 using Geometric_Calculator.Models.Quadrangles.Parallelograms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,11 +9,20 @@ namespace Geometric_Calculator.Pages.Quadrangles.Parallelograms
     {
         public string? Message { get; private set; }
 
-        public void OnPost(double side, double diagonal)
+        public void OnPost(string side, string diagonal)
         {
-            Square square = new(side, diagonal);
+            try
+            {
+                double[] values = Settings.GetValues(new string[] { side, diagonal });
 
-            Message = $"Area = '{square.GetArea()}'";
+                Square square = new(values[0], values[1]);
+
+                Message = $"Area = '{square.GetArea()}'";
+            }
+            catch (Exception exc)
+            {
+                Message = exc.Message;
+            }
         }
     }
 }
--- Geometric Calculator/Pages/Quadrangles/Trapezoids/Trapezoid.cshtml.cs
+++ Geometric Calculator/Pages/Quadrangles/Trapezoids/Trapezoid.cshtml.cs
@@ -16,7 +16,7 @@ namespace Geometric_Calculator.Pages.Quadrangles.Trapezoids
 
                 Trapezoid trapezoid = new(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9], values[10], values[11], values[12], values[13]);
 
-                Message = Message = $"Area = '{trapezoid.GetArea()}'";
+                Message = $"Area = '{trapezoid.GetArea()}'";
             }
             catch (Exception exc)
             {
