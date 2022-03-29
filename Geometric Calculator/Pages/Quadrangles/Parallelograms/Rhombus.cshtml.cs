6f13958b1e5d3e5c600fa53ee5c879f25b80bebd
m1adow <Vell0505189970sl@gmail.com>
2022-03-29 22:39:28 +0300

    Added rhombus handler

--- Geometric Calculator/Pages/Quadrangles/Parallelograms/Rhombus.cshtml.cs
+++ Geometric Calculator/Pages/Quadrangles/Parallelograms/Rhombus.cshtml.cs
@@ -1,5 +1,5 @@
+using Geometric_Calculator.Models;
 using Geometric_Calculator.Models.Quadrangles.Parallelograms;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Geometric_Calculator.Pages.Quadrangles.Parallelograms
@@ -8,11 +8,20 @@ namespace Geometric_Calculator.Pages.Quadrangles.Parallelograms
     {
         public string? Message { get; private set; }
 
-        public void OnPost(double side, double diagonalA, double diagonalB, double heightA, double heightB, double angleA, double angleB)
+        public void OnPost(string side, string diagonalA, string diagonalB, string heightA, string heightB, string angleA, string angleB)
         {
-            Rhombus rhombus = new(side, diagonalA, diagonalB, heightA, heightB, angleA, angleB);
+            try
+            {
+                double[] values = Settings.GetValues(new string[] { side, diagonalA, diagonalB, heightA, heightB, angleA, angleB });
 
-            Message = $"Area = '{rhombus.GetArea()}'";
+                Rhombus rhombus = new(values[0], values[1], values[2], values[3], values[4], values[5], values[6]);
+
+                Message = $"Area = '{rhombus.GetArea()}'";
+            }
+            catch (Exception exc)
+            {
+                Message = exc.Message;
+            }
         }
     }
 }
