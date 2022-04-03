4f6c8a278a725e1ebd20ebe8c62e8d7981764f92
m1adow <Vell0505189970sl@gmail.com>
2022-04-03 23:28:40 +0300

    Added full addition

--- Geometric Calculator/Models/Settings.cs
+++ Geometric Calculator/Models/Settings.cs
@@ -4,7 +4,7 @@ public class Settings
 {
     public static double[] GetValues(string[] values)
     {
-        char[] actions = new char[] { '+', '-', '*', '/' }; //action chars
+        string[] actions = new string[] { "+", "-", "*", "/" }; //action chars
 
         //change dot to comma
         while (values.Any(v => v.Any(c => c == '.')))
@@ -20,45 +20,47 @@ public class Settings
         }
 
         //examination for containing not numbers inside string
-        if (values.Any(v => v.Any(c => c != ',' && !char.IsDigit(c) && !actions.Contains(c)))) throw new Exception("You must enter only numbers");
+        if (values.Any(v => v.Any(c => c != ',' && !char.IsDigit(c) && !actions.Contains(c.ToString())))) throw new Exception("You must enter only numbers");
 
-        if (values.Any(v => v.Any(c => actions.Contains(c))))
+        if (values.Any(v => v.Any(c => actions.Contains(c.ToString()))))
         {
             List<string> valuesWithActions = values.Where(v => v.Any(c => c == '*' || c == '/' || c == '+' || c == '-')).ToList(); //collection with values which have actions
 
             valuesWithActions.ForEach(x => Console.WriteLine(x));
 
-            foreach (var item in valuesWithActions)
+            for (int i = 0; i < valuesWithActions.Count; i++)
             {
-                int indexOfValueWithAction = Array.IndexOf(values, item); //index of item in original array
-
-                char[] splitedValuesWithActions = item.Trim(' ').ToCharArray(); //split string with action
+                List<string> splitedValuesWithActions = valuesWithActions[i].Trim(' ').Select(x => new string(x, 1)).ToList(); //split string with action
 
                 splitedValuesWithActions.ToList().ForEach(x => Console.WriteLine(x));
 
                 //20 + 3 * 80 * 9
 
-                while (splitedValuesWithActions.Any(s => actions.Contains(s)))
+                while (splitedValuesWithActions.Any(c => actions.Contains(c.ToString())))
                 {
-                    var containingActions = actions.Where(a => splitedValuesWithActions.Any(s => s == a)).ToList(); //find action chars which string has
+                    List<string>? containingActions = actions.Where(a => valuesWithActions[i].Any(c => c.ToString() == a)).ToList(); //find action chars which string has
                     ActionSort(containingActions); //sort by action
 
                     containingActions.ToList().ForEach(x => Console.WriteLine(x));
 
-                    foreach (var action in containingActions)
+                    for (int j = 0; j < containingActions.Count; j++)
                     {
-                        int indexOfSplitedValueWithAction = Array.IndexOf(splitedValuesWithActions, action); //index of splited value with action
+                        int indexOfSplitedValueWithAction = Array.IndexOf(splitedValuesWithActions.ToArray(), containingActions[j]); //index of splited value with action
 
-                        if (action == '+') values[indexOfValueWithAction] = (double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) + double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString())).ToString(); //splitedValuesWithActions[indexOfSplitedValueWithAction - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) + double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString()));
-                        else if (action == '-') splitedValuesWithActions[indexOfSplitedValueWithAction - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) - double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString()));
-                        else if (action == '*') splitedValuesWithActions[indexOfSplitedValueWithAction - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) * double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString()));
-                        else if (action == '/') splitedValuesWithActions[indexOfSplitedValueWithAction - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) / double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString()));
+                        if (containingActions[j] == "+") splitedValuesWithActions[indexOfSplitedValueWithAction] = (double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction - 1].ToString()) + double.Parse(splitedValuesWithActions[indexOfSplitedValueWithAction + 1].ToString())).ToString();
+                        //else if (containingActions[j] == '-') splitedValuesWithActions[j - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[j - 1].ToString()) - double.Parse(splitedValuesWithActions[j + 1].ToString()));
+                        //else if (containingActions[j] == '*') splitedValuesWithActions[j - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[j - 1].ToString()) * double.Parse(splitedValuesWithActions[j + 1].ToString()));
+                        //else if (containingActions[j] == '/') splitedValuesWithActions[j - 1] = Convert.ToChar(double.Parse(splitedValuesWithActions[j - 1].ToString()) / double.Parse(splitedValuesWithActions[j + 1].ToString()));
 
                         ClearArrayAfterAction(splitedValuesWithActions, indexOfSplitedValueWithAction); //clear unnecessary elements
+                        splitedValuesWithActions.ToList().ForEach(x => Console.WriteLine(x));                                                                
                     }
                 }
 
+                values[i] = splitedValuesWithActions.FirstOrDefault(s => s != string.Empty);
+
                 splitedValuesWithActions.ToList().ForEach(x => Console.WriteLine(x));
+                values.ToList().ForEach(x => Console.WriteLine(x));
             }
         }
 
@@ -69,26 +71,28 @@ public class Settings
         return result;
     }
 
-    private static void ClearArrayAfterAction(char[] array, int index)
+    private static void ClearArrayAfterAction(List<string> collection, int index)
     {
-        array[index] = '\0';
-        array[index + 1] = '\0';
+        collection[index - 1] = string.Empty;
+        collection[index + 1] = string.Empty;
+
+        collection.RemoveAll(s => s == string.Empty);
     }
 
-    private static void ActionSort(List<char>? actions)
+    private static void ActionSort(List<string>? actions)
     {
         //sort array with preferable action
         for (int i = 0; i < actions?.Count; i++)
         {
-            if (actions[i] == '*') ChangePosition(actions, i, actions[i]);
-            else if (actions[i] == '/') ChangePosition(actions, i, actions[i]);
+            if (actions[i] == "*") ChangePosition(actions, i, actions[i]);
+            else if (actions[i] == "/") ChangePosition(actions, i, actions[i]);
         }
     }
 
-    private static void ChangePosition(List<char>? actions, int index, char action)
+    private static void ChangePosition(List<string>? actions, int index, string action)
     {
         //examination for availability of preferable action at first place 
-        if (actions[0] == '*' || actions[0] == '/')
+        if (actions[0] == "*" || actions[0] == "/")
         {
             actions[index] = actions[1];
             actions[1] = action;
