31391b5d0ca2661374b56e7001054c38ee8a8771
Valentin Maschenko <m1adow1654@gmail.com>
2022-05-15 15:56:52 -0700

    Added unit tests for triangle

---
+++ GeometricCalculatorNUnitTests/TriangleTests.cs
@@ -0,0 +1,52 @@
+using Geometric_Calculator.Models.Triangles;
+
+namespace GeometricCalculatorNUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestDefaultTriangleWithSinFormula()
+        {
+            double firstSide = 5;
+            double secondSide = 4;
+
+            double thirdAngle = 30;
+
+            Triangle triangle = new(firstSide, secondSide, 0, 0, 0, 0, 0, 0, thirdAngle);
+
+            //0.5 * 5 * 4 * sin(30)
+            Assert.AreEqual(0.5 * 5 * 4 * Math.Sin(30 * Math.PI / 180), triangle.GetArea());
+        }
+
+        [Test]
+        public void TestDefaultTriangleWithGeronFormula()
+        {
+            double firstSide = 5;
+            double secondSide = 6;
+            double thirdSide = 9;
+
+            Triangle triangle = new(firstSide, secondSide, thirdSide, 0, 0, 0, 0, 0, 0);
+
+            //(9 + 5 + 6) / 2 = 10
+            //sqrt(10(10-9)(10-5)(10-6))
+            Assert.AreEqual(Math.Sqrt(10 * (10 - 9) * (10 - 5) * (10 - 6)), triangle.GetArea());
+        }
+
+        [Test]
+        public void TestDefaultTriangleWithHeightFormula()
+        {
+            double firstSide = 5;
+            double firstHeight = 10;
+
+            Triangle triangle = new(firstSide, 0, 0, firstHeight, 0, 0, 0, 0, 0);
+
+            //0.5 * 5 * 10
+            Assert.AreEqual(0.5 * 5 * 10, triangle.GetArea());
+        }
+    }
+}
